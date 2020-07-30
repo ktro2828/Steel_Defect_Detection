c753dcb4be727f7f2af3d33f66c18308e18dec61
kotarouetake <ktro310115@gmail.com>
2020-07-31 03:11:46 +0900

    Update

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 import os
 import os.path as osp
 
-import argparse
+# import argparse
 import numpy as np
 import torch
 
@@ -13,33 +13,35 @@ from trainer import Trainer
 from unet import UNet
 
 
-def main():
+def main(args):
     seed = 1234
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--loss', type=str,
-                        help='BCE, Dice, DiceBCE, IoU, Tanimoto')
-
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-l', '--loss', type=str,
+    #                     help='BCE, Dice, DiceBCE, IoU, Tanimoto')
+    #
+    # args = parser.parse_args()
 
     if osp.exists('../trained_models') is False:
         os.makedirs('../trained_models')
 
     model = UNet(3, 4)
-    model_trainer = Trainer(model, loss=args.loss)
+    model_trainer = Trainer(model, loss=args[-1])
     model_trainer.start()
 
     losses = model_trainer.losses
     dice_scores = model_trainer.dice_scores
     iou_scores = model_trainer.iou_scores
 
-    plot(losses, name='{} Loss'.format(args.loss))
+    plot(losses, name='{} Loss'.format(args[-1]))
     plot(dice_scores, name='Dice score')
     plot(iou_scores, 'IoU score')
 
 
 if __name__ == '__main__':
-    main()
+    import sys
+    argv = sys.argv
+    main(argv)
